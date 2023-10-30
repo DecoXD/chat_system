9c1083a841b898af26cc5ad05dab47d22a86f739
Andre Pinheiro <deco.xd@outlook.com>
2023-10-29 21:29:19 -0300

    added the core funcionalites

--- routes/MessagesRoutes.js
+++ routes/MessagesRoutes.js
@@ -3,6 +3,8 @@ const Message = require('../models/Message')
 const router = require('express').Router()
 
 
+router.get('/:receiver',Message.show_messages)
+
 router.post('/:receiver',Message.send_message)
 
 
